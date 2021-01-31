c29ea1444d9af97509994cc142463df5a9f0003e
Aaron Toth <atoth89@gmail.com>
2021-01-31 01:17:38 -0500

    Make a poetry-flavoured pyproject when generating a new project

--- courant/project.py
+++ courant/project.py
@@ -15,14 +15,29 @@ def create(path: str) -> Project:
 
     project_conf = document()
 
+    poetry_meta = table()
+    poetry_meta.add("name", os.path.dirname(path))
+    poetry_meta.add("version", "0.0.1")
+    poetry_meta.add("description", "Just an opinionated photo stream site")
+    project_conf.add("tool.poetry", poetry_meta)
+
+    poetry_deps = table()
+    poetry_deps.add("python", "^3.8")
+    poetry_deps.add("courant", "*")
+    project_conf.add("tool.poetry.dependencies", poetry_deps)
+
+    build_sys = table()
+    build_sys.add("requires", ["poetry>=0.12"])
+    build_sys.add("build-backend", "poetry.masonry.api")
+    project_conf.add("build-system", build_sys)
+
     meta = table()
     meta.add("name", os.path.dirname(path))
     meta.add("author", "John Smith")
     meta.add("author_bio", "Just a photographer")
-
     project_conf.add("meta", meta)
 
-    with open(os.path.join(path, "project.toml"), "w", encoding="utf-8") as f:
+    with open(os.path.join(path, "pyproject.toml"), "w", encoding="utf-8") as f:
         f.write(project_conf.as_string())
 
     return Project(path)
