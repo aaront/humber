6e6460bc833ef57ab0f18539d57c0bbc4ffb07bb
Aaron Toth <atoth89@gmail.com>
2021-02-20 21:29:12 -0500

    Add AVIF output

--- humber/engines/image.py
+++ humber/engines/image.py
@@ -1,22 +1,22 @@
 import os.path
-from dataclasses import dataclass
 from pathlib import Path
-from typing import List
+from typing import Dict, List, TypedDict
 
+import pillow_avif  # noqa: F401
 from PIL import Image
 
 from . import Engine
 
+DEFAULT_IMAGE_QUALITY = 80
 
-@dataclass
-class ImageOutputMeta:
+
+class ImageOutputMeta(TypedDict):
     path: Path
     format: str
     width: int
 
 
-@dataclass
-class ImageMeta:
+class ImageMeta(TypedDict):
     original_path: Path
     outputs: List[ImageOutputMeta]
 
@@ -24,17 +24,20 @@ class ImageMeta:
 class ImageEngine(Engine):
     widths: List[int]
     formats: List[str]
+    qualities: Dict[str, int]
     output_path: Path
 
     def __init__(
         self,
         widths: List[int] = [300, 1000],
-        formats: List[str] = ["webp", "jpg"],
+        formats: List[str] = ["webp", "avif", "jpg"],
+        qualities: Dict[str, int] = {"avif": 70},
         output_path=None,
     ) -> None:
         super().__init__()
         self.widths = widths
         self.formats = formats
+        self.qualities = qualities
         self.output_path = output_path
 
     def generate(self, fp: Path) -> ImageMeta:
@@ -46,13 +49,29 @@ class ImageEngine(Engine):
         for width in self.widths:
             w_ratio = width / float(source_w)
             height = int(source_h * w_ratio)
-            source_image.resize((width, height))
+            scaled_image = source_image.resize((width, height))
             filename, _ = os.path.splitext(os.path.basename(source_image.filename))
             for format in self.formats:
                 output_path = self.output_path / f"{filename}.{width}w.{format}"
-                source_image.save(output_path)
-                image_outputs.append(ImageOutputMeta(output_path, format, width))
-        return ImageMeta(fp, image_outputs)
+                quality = self.qualities.get(format) or DEFAULT_IMAGE_QUALITY
+                scaled_image.save(
+                    output_path,
+                    quality=quality,
+                    speed=0,  # AVIF
+                    method=6,  # WebP
+                    **_get_avif_qmin_max(quality),
+                )
+                image_outputs.append(
+                    ImageOutputMeta(path=output_path, format=format, width=width)
+                )
+        return ImageMeta(original_path=fp, outputs=image_outputs)
 
     def generate_html(self, fp: Path, template: str = None) -> str:
         pass
+
+
+def _get_avif_qmin_max(quality: int) -> Dict[str, int]:
+    return {
+        "qmin": max(0, min(64 - quality, 63)),
+        "qmax": max(0, min(100 - quality, 63)),
+    }
--- tests/test_engines_image.py
+++ tests/test_engines_image.py
@@ -7,4 +7,4 @@ def test_generate_images(tmp_path: Path):
     input_file = Path(__file__).resolve().parent / "images" / "IMGP8501.jpg"
     ImageEngine(output_path=tmp_path).generate(input_file)
     files = list(tmp_path.iterdir())
-    assert len(files) == 4
+    assert len(files) == 6
