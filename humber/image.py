5983c01644b3e468a5e5ab4f0af38ee2b8ee4b2a
Aaron Toth <atoth89@gmail.com>
2021-02-27 18:58:08 -0500

    Added responsive image template and rendering

--- humber/image.py
+++ humber/image.py
@@ -1,11 +1,23 @@
 import os.path
 from pathlib import Path
-from typing import Dict, List, TypedDict
+from typing import Dict, List, Optional, TypedDict
 
 import pillow_avif  # noqa: F401
+from jinja2 import Markup
 from PIL import Image as _Image
 
-DEFAULT_IMAGE_QUALITY = 80
+from jinja2 import Environment, FileSystemLoader
+
+_IMAGE_QUALITIES = {
+    "default": 80,
+    "avif": 70,
+}
+
+_IMAGE_MIMETYPES = {
+    "avif": "image/avif",
+    "webp": "image/webp",
+    "jpg": "image/jpeg",
+}
 
 
 class ImageOutputMeta(TypedDict):
@@ -22,14 +34,19 @@ class ImageMeta(TypedDict):
 class Image:
     _image: _Image
     filename: str
+    ext: str
+    url: str
 
-    def __init__(self, path: Path) -> None:
+    def __init__(self, path: Path, url: str) -> None:
         if not path or not path.exists:
             raise TypeError("Image path was not provided")
         if not path.exists():
             raise ValueError(f"Image path '{path}' does not exist")
         self._image = _Image.open(path)
-        self.filename, _ = os.path.splitext(os.path.basename(self._image.filename))
+        self.filename, self.ext = os.path.splitext(
+            os.path.basename(self._image.filename)
+        )
+        self.url = url
 
     def scale_width(self, max_width: int) -> _Image:
         source_w, source_h = self._image.size
@@ -38,44 +55,53 @@ class Image:
         return self._image.resize((max_width, max_height))
 
 
-class ResponsiveImage(Image):
-    widths: List[int]
-    formats: List[str]
-    qualities: Dict[str, int]
-
-    def __init__(
-        self,
-        path: Path,
-        widths: List[int] = [300, 1000],
-        formats: List[str] = ["webp", "avif", "jpg"],
-        qualities: Dict[str, int] = {"avif": 70},
-    ) -> None:
-        super().__init__(path)
-        self.widths = widths
-        self.formats = formats
-        self.qualities = qualities
-
-    def generate(self, output_path: Path):
-        image_outputs: List[ImageOutputMeta] = []
-        for width in self.widths:
-            scaled_image = self.scale_width(width)
-            for format in self.formats:
-                output_file = output_path / f"{self.filename}.{width}w.{format}"
-                quality = self.qualities.get(format) or DEFAULT_IMAGE_QUALITY
-                scaled_image.save(
-                    output_file,
-                    quality=quality,
-                    speed=0,  # AVIF
-                    method=6,  # WebP
-                    **_get_avif_qmin_max(quality),
-                )
-                image_outputs.append(
-                    ImageOutputMeta(path=output_file, format=format, width=width)
-                )
-        return ImageMeta(original_path=output_file, outputs=image_outputs)
+def generate_srcsets(
+    image: Image,
+    output_path: Path,
+    format: str,
+    widths: List[int] = [300, 1000],
+    quality: int = None,
+):
+    srcsets: List[str] = []
+    for width in sorted(widths, reverse=True):
+        scaled_image = image.scale_width(width)
+        output_filename = f"{image.filename}.{width}w.{format}"
+        output_file = output_path / output_filename
+        image_quality = quality or _IMAGE_QUALITIES.get(
+            format, _IMAGE_QUALITIES.get("default")
+        )
+        scaled_image.save(
+            output_file,
+            quality=image_quality,
+            speed=0,  # AVIF
+            method=6,  # WebP
+            **_get_avif_qmin_max(image_quality),
+        )
+        srcsets.append(f"{image.url}/{output_filename} {width}w")
+    sizes = "(max-width: 608px) 100vw, 608px"
+    image_type = _IMAGE_MIMETYPES[format]
+    return Markup(f'sizes="{sizes}" srcset="{", ".join(srcsets)}" type="{image_type}"')
 
 
-def _get_avif_qmin_max(quality: int) -> Dict[str, int]:
+env = Environment(
+    loader=FileSystemLoader(Path(__file__).resolve().parent / "templates"),
+    enable_async=True,
+)
+env.filters["generate_srcsets"] = generate_srcsets
+
+
+async def render_image(
+    image: Image, output_path: Path, formats: List[str], widths: List[int]
+):
+    template = env.get_template("image.html")
+    return await template.render_async(
+        image=image, output_path=output_path, formats=formats, widths=widths
+    )
+
+
+def _get_avif_qmin_max(quality: Optional[int]) -> Dict[str, int]:
+    if not quality:
+        return {}
     return {
         "qmin": max(0, min(64 - quality, 63)),
         "qmax": max(0, min(100 - quality, 63)),
--- tests/test_image.py
+++ tests/test_image.py
@@ -2,20 +2,52 @@ import pytest
 
 from pathlib import Path
 
-from humber.image import ResponsiveImage
+from humber.image import Image, generate_srcsets, render_image
 
 
 def test_image():
     with pytest.raises(TypeError) as typeerror:
-        ResponsiveImage(None)
+        Image(None, "")
     assert "not provided" in str(typeerror.value)
     with pytest.raises(ValueError) as valueerror:
-        ResponsiveImage(Path(__file__).resolve().parent / "images" / "doesntexist.jpg")
+        Image(Path(__file__).resolve().parent / "images" / "doesntexist.jpg", "")
     assert "does not exist" in str(valueerror.value)
 
 
-def test_generate(tmp_path: Path):
+def test_generate_srcsets(tmp_path: Path):
     input_file = Path(__file__).resolve().parent / "images" / "IMGP8501.jpg"
-    ResponsiveImage(input_file, widths=[150, 300]).generate(output_path=tmp_path)
-    files = list(tmp_path.iterdir())
-    assert len(files) == 6
+    image = Image(input_file, "posts/2021")
+    srcsets = generate_srcsets(image, tmp_path, format="webp", widths=[150, 300])
+    assert (
+        srcsets
+        == 'sizes="(max-width: 608px) 100vw, 608px" srcset="posts/2021/IMGP8501.300w.webp 300w, posts/2021/IMGP8501'
+        '.150w.webp 150w" type="image/webp"'
+    )
+
+
+@pytest.mark.asyncio
+async def test_render_image(tmp_path: Path):
+    input_file = Path(__file__).resolve().parent / "images" / "IMGP8501.jpg"
+    image = Image(input_file, "posts/2021")
+    rendered_html = await render_image(
+        image=image,
+        output_path=tmp_path,
+        formats=["avif", "webp", "jpg"],
+        widths=[150, 300],
+    )
+    assert (
+        'srcset="posts/2021/IMGP8501.300w.avif 300w, posts/2021/IMGP8501.150w.avif 150w" type="image/avif"'
+        in rendered_html
+    )
+    assert (
+        'srcset="posts/2021/IMGP8501.300w.avif 300w, posts/2021/IMGP8501.150w.avif 150w" type="image/avif"'
+        in rendered_html
+    )
+    assert (
+        'srcset="posts/2021/IMGP8501.300w.avif 300w, posts/2021/IMGP8501.150w.avif 150w" type="image/avif"'
+        in rendered_html
+    )
+    assert (
+        'src="posts/2021/IMGP8501.jpg" style="background-size: cover; background-image: none;" decoding="async" '
+        'loading="lazy"' in rendered_html
+    )
